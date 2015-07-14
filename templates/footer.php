78860afc81b7538cb4413afe734284482776ca28
Matt Coleman <hello@matt-coleman.co.uk>
2015-07-14 22:03:04 +0100

    Added basic footer content and styles

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,16 @@
-<footer class="content-info" role="contentinfo">
+<footer id="footer" class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+      <div class="col-sm-3">
+        <p>Left</p>
+      </div>
+      <div class="col-sm-6">
+        <p>Middle</p>
+      </div>
+      <div class="col-sm-3">
+        <p>Right</p>
+      </div>
+    </div>
+    <?php //dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
