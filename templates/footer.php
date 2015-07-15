5a7e69b77937c5d99fc132f08bb342b54da02827
Matt Coleman <hello@matt-coleman.co.uk>
2015-07-15 23:49:24 +0100

    Added footer content and styles

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,16 @@
       <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
   </div>
-  <div class="container dark-blue">
-    <p>&copy; <?php bloginfo('name'); ?> All rights reserved.</p>
+  <div class="footer-bottom dark-blue">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <p>&copy; <?php bloginfo('name'); ?> All rights reserved.</p>
+        </div>
+        <div class="col-sm-6">
+          <?php echo display_ultimate_social_icons();?>
+        </div>
+      </div>
+    </div>
   </div>
 </footer>
