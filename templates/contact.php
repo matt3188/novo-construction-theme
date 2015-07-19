76101523a32142cd945b93c0dbefaebf79e74006
Matt Coleman <hello@matt-coleman.co.uk>
2015-07-19 22:55:56 +0100

    Created social navigation and styled it up on contact page. Added phone number in special style

--- templates/contact.php
+++ templates/contact.php
@@ -10,18 +10,20 @@ the_post(); ?>
     $secondColumn = get_field('second_column');
   ?>
 <div class="col-sm-6">
-  <h2 class="heading main-heading">Contact us</h2>
+  <h2 class="heading sub-heading">Contact us</h2>
   <p><?php echo $firstColumn ?></p>
   <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form"]'); ?>
 </div>
 <div class="col-sm-6">
-  <h2 class="heading main-heading">Get in Touch</h2>
-    <p><?php echo $secondColumn ?></p>
-    <nav class="navigation social-navigation" role="navigation">
-      <?php
-      if (has_nav_menu('social_navigation')) :
-        wp_nav_menu(['theme_location' => 'social_navigation', 'container' => false, 'menu_class' => 'list horizontal-list']);
-      endif;
-      ?>
-    </nav>
+  <h2 class="heading sub-heading">Get in Touch</h2>
+  <p><?php echo $secondColumn ?></p>
+  <nav class="navigation social-navigation large-icons" role="navigation">
+    <?php
+    if (has_nav_menu('social_navigation')) :
+      wp_nav_menu(['theme_location' => 'social_navigation', 'container' => false, 'menu_class' => 'list horizontal-list']);
+    endif;
+    ?>
+  </nav>
+  <h3 class="heading sub-heading grey">Available from 9am - 6pm</h3>
+  <h3 class="heading special-heading">01920 823 006</h3>
 </div>
