0b84cfc4776dcc50b1e47070ae313da2e58a1ca6
Matt Coleman <hello@matt-coleman.co.uk>
2015-07-19 13:06:56 +0100

    Added div with class of row to remove padding left and right

--- templates/project-listing.php
+++ templates/project-listing.php
@@ -15,7 +15,6 @@ the_post(); ?>
   <ul class="list horizontal-list category-list">
     <button class="category is-active" data-filter="*">Show all</button>
     <?php while(have_rows('project')): the_row(); ?>
-      <!-- Pull out choice labels -->
       <?php
         $field = get_sub_field_object('project_category');
         $value = get_sub_field('project_category');
@@ -26,31 +25,33 @@ the_post(); ?>
   </ul>
 </nav>
 
-<div class="isotope">
-  <ul class="list horizontal-list">
-    <?php if (have_rows('project')): ?>
-      <?php while (have_rows('project')): the_row(); ?>
-      <?php
-        $heading = get_sub_field('project_title');
-        $description = get_sub_field('project_description');
-        $thumbnail = get_sub_field('project_thumbnail');
-        $category = get_sub_field('project_category');
-        $link = get_sub_field('project_link');
-      ?>
-      <li class="col-sm-4 isotope-item project-block <?php echo $category; ?>">
-        <div class="project">
-          <div class="project-content">
-            <a href="<?php echo $link; ?>" class="project-thumb" style="background-image: url('<?php echo $thumbnail['url']; ?>');">
-              <div class="thumb-overlay">
-                <i class="fa fa-plus"></i>
-              </div>
-            </a>
-            <h4 class="heading project-heading"><?php echo $heading; ?></h4>
-            <?php echo $description; ?>
+<div class="row">
+  <div class="isotope">
+    <ul class="list horizontal-list">
+      <?php if (have_rows('project')): ?>
+        <?php while (have_rows('project')): the_row(); ?>
+        <?php
+          $heading = get_sub_field('project_title');
+          $description = get_sub_field('project_description');
+          $thumbnail = get_sub_field('project_thumbnail');
+          $category = get_sub_field('project_category');
+          $link = get_sub_field('project_link');
+        ?>
+        <li class="col-sm-4 isotope-item project-block <?php echo $category; ?>">
+          <div class="project">
+            <div class="project-content">
+              <a href="<?php echo $link; ?>" class="project-thumb" style="background-image: url('<?php echo $thumbnail['url']; ?>');">
+                <div class="thumb-overlay">
+                  <i class="fa fa-plus"></i>
+                </div>
+              </a>
+              <h4 class="heading project-heading"><?php echo $heading; ?></h4>
+              <?php echo $description; ?>
+            </div>
           </div>
-        </div>
-      </li>
-      <?php endwhile; ?>
-    <?php endif; ?>
-  </ul>
+        </li>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </ul>
+  </div>
 </div>
