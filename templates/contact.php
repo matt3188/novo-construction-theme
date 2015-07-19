cf5a506121f5c0ccc721b6d1b2f0b4b69fa58247
Matt Coleman <hello@matt-coleman.co.uk>
2015-07-19 22:54:40 +0100

    Removed iframe from contact page

--- templates/contact.php
+++ templates/contact.php
@@ -5,8 +5,6 @@
 get_header();
 the_post(); ?>
 
-<!-- <iframe class="map-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2462.7664667567215!2d0.025587599999991106!3d51.8834753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d882ed69c2a715%3A0x745878b87e3a2ef5!2sNovo+Construction!5e0!3m2!1sen!2suk!4v1437080717377" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
-
 <?php
     $firstColumn = get_field('first_column');
     $secondColumn = get_field('second_column');
