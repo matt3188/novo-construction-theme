8d7b0a725b7982636e197150227e243afc764fb6
Matt Coleman <hello@matt-coleman.co.uk>
2015-08-11 22:52:26 +0100

    Added Owl carousel to manifest and formatted responsive attributes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -81,17 +81,10 @@
     autoplay: true,
     autoplayTimeout: 3000,
     responsive: {
-      0: {
-          items: 1
-      },
-      600: {
-          items: 3
-      },
-      1000: {
-          items: 5
-      }
+      0: { items: 1 },
+      600: { items: 3 },
+      1000: { items: 5 }
     }
-
   });
 
   // Mobile menu trigger
