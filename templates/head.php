ba3e2fe7d4c222d5cb40d551b0430686e8326ce3
Matt Coleman <hello@matt-coleman.co.uk>
2015-09-08 00:44:48 +0100

    Included Font Awesome globally and favicon

--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <?php if(is_front_page()): ?>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-  <?php endif; ?>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
 </head>
