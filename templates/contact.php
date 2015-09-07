0afe3ab50f8c7484746288727b21d8b42f68e4c6
Matt Coleman <hello@matt-coleman.co.uk>
2015-09-08 00:44:26 +0100

    Removed social menu and large phone number from contact us page, also added back in contact form

--- templates/contact.php
+++ templates/contact.php
@@ -12,20 +12,9 @@ the_post(); ?>
 <div class="col-sm-6">
   <h2 class="heading sub-heading">Contact us</h2>
   <?php echo $firstColumn ?>
-  <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form"]'); ?>
 </div>
 <div class="col-sm-6">
   <h2 class="heading sub-heading">Get in Touch</h2>
   <?php echo $secondColumn ?>
-  <nav class="navigation social-navigation large-icons" role="navigation">
-    <?php
-    if (has_nav_menu('social_navigation')) :
-      wp_nav_menu(['theme_location' => 'social_navigation', 'container' => false, 'menu_class' => 'list horizontal-list']);
-    endif;
-    ?>
-  </nav>
-  <div class="company-info">
-    <h3 class="heading sub-heading grey">Available from 9am - 6pm</h3>
-    <h3 class="heading special-heading"><a href="tel:01920 823 006">01920 823 006</a></h3>
-  </div>
+  <?php echo do_shortcode('[contact-form-7 id="332" title="Contact form"]'); ?>
 </div>
