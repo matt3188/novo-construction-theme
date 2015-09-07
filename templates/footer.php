55f64e72f6d9d915bc89e39fa8cf19e5e0aaf7a9
Matt Coleman <hello@matt-coleman.co.uk>
2015-09-08 00:46:00 +0100

    Added Google analytics code

--- templates/footer.php
+++ templates/footer.php
@@ -26,3 +26,15 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.1/isotope.pkgd.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+
+<!-- TODO - look for more elegant way to include this -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63280687-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
